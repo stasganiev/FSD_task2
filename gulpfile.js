7e23a979349c4a1cb50434e1dd83789d19053725
GanievStas <benony0gmail.com>
2020-05-10 06:46:50 +1000

    Причесал архитектуру

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ gulp.task("fonts", ()=>{
 });
 
 gulp.task("img", ()=>{
-  return gulp.src(["src/img/**/*.*", "src/components/**/*.svg", "src/components/**/*.jpg", "src/components/**/*.png"])
+  return gulp.src(["src/img/**/*.*", "src/elements/**/*.svg", "src/elements/**/*.jpg", "src/elements/**/*.png"])
     .pipe(gulp.dest("public/img"));
 });
 
@@ -56,7 +56,7 @@ gulp.task("server", ()=>{
     ui: false
   });
 
-  gulp.watch("src/components/**/*.*", gulp.series("img", "js", "pug", "css"));
+  gulp.watch("src/elements/**/*.*", gulp.series("img", "js", "pug", "css"));
   gulp.watch("src/fonts/**/*.*", gulp.series("fonts"));
   gulp.watch("src/img/**/*.*", gulp.series("img"));
   gulp.watch("src/js/**/*.js", gulp.series("js"));
